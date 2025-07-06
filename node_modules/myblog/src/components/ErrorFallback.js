2bcf2e6323cf51c6b959550771b1c6f4aba8c654
Noah-Wallace <noahwallace9044@gmail.com>
2025-07-07 03:33:45 +0530

    commit

---
+++ node_modules/myblog/src/components/ErrorFallback.js
@@ -0,0 +1,11 @@
+// frontend/src/components/ErrorFallback.js
+import React from 'react';
+
+const ErrorFallback = () => (
+  <div style={{ padding: 20 }}>
+    <h2>Something went wrong.</h2>
+    <p>Please try refreshing the page.</p>
+  </div>
+);
+
+export default ErrorFallback;
